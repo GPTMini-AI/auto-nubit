ff49b6c3236b808c68f78ab74c7d4eb526e28bf3
GPTMini-AI <144501251+GPTMini-AI@users.noreply.github.com>
2024-07-08 17:36:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,7 +13,15 @@ while ! grep -q "AUTH KEY" $HOME/nubit-light.log; do
 done
 
 # 提取NAME到AUTH KEY及其下一行的内容，并保存到文件
-awk '/NAME/{flag=1} /AUTH KEY/{print; getline; print; flag=0} flag' $HOME/nubit-light.log > $HOME/nubit-public-key.txt
+awk '
+/NAME/ { flag=1 }
+/AUTH KEY/ {
+  if (flag == 1) {
+    print; getline; print; flag=0
+  }
+}
+flag
+' $HOME/nubit-light.log > $HOME/nubit-public-key.txt
 
 # 输出提取的内容到控制台
 cat $HOME/nubit-public-key.txt
